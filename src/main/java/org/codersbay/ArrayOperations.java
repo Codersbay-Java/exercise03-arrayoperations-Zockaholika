c16e5091c342db93cb18d84772d23ac4de6ae6f3
Zockaholika <113764832+Zockaholika@users.noreply.github.com>
2022-10-25 17:26:19 +0200

    251020221726

--- src/main/java/org/codersbay/ArrayOperations.java
+++ src/main/java/org/codersbay/ArrayOperations.java
@@ -1,10 +1,12 @@
 package org.codersbay;
 
+import java.util.Arrays;
+
 /**
  * Utility class which performs various operations with integer arrays.
  */
 public class ArrayOperations {
-    private final int[] numbers;      // array that needs to be sorted.
+    private int[] numbers;      // array that needs to be sorted.
     private final int[] unsorted;     // reference array which stays unsorted.
 
     public ArrayOperations(int[] numbers) {
@@ -30,21 +32,24 @@ public class ArrayOperations {
      * @see <a href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
      */
     public int[] sort() {
-        return null;
+        Arrays.sort(numbers);
+        return numbers;
     }
 
     /**
      * @return the sorted array in reverse order
      */
     public int[] revertSort() {
-        return null;
+        sort();
+        return reverseArray();
     }
 
     /**
      * @return the unsorted array in reverted order.
      */
     public int[] reverted() {
-        return null;
+        reverseArray();
+        return numbers;
     }
 
     /**
@@ -52,35 +57,53 @@ public class ArrayOperations {
      * @return true if the array contains the value, false otherwise.
      */
     public boolean contains(int value) {
-        return false;
+        boolean boolForReturn = false;
+        for (int i = 0; i < numbers.length; i++) {
+            if (value == numbers[i]){
+                boolForReturn = true;
+            }
+        }
+        return boolForReturn;
     }
 
     /**
      * @return the average value of all elements summed up.
      */
     public double average() {
-        return 0.0;
+        double sum = 0.0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        sum = sum / numbers.length;
+        return sum;
     }
 
     /**
      * @return the average value of all elements summed up, but without the highest and the lowest value.
      */
     public double trimmedMean() {
-        return 0.0;
+        sort();
+        double sum = 0;
+        for (int i = 1; i < numbers.length - 1 ; i++) {
+            sum = sum +numbers[i];
+        }
+        return sum / (numbers.length-2);
     }
 
     /**
      * @return the max value of the array. In the array [1,9,3] max would be 9.
      */
     public int maxValue() {
-        return 0;
+        sort();
+        return numbers[numbers.length-1];
     }
 
     /**
      * @return the min value of the array. In the array [1,9,3] min would be 1.
      */
     public int minValue() {
-        return 0;
+        sort();
+        return numbers[0];
     }
 
     /* **********************************************************
@@ -88,7 +111,14 @@ public class ArrayOperations {
      *   you like.                                               *
      * ***********************************************************/
 
-    private int someHelper(int[] tmp) {
-        return 1;
+    public int[] reverseArray() {
+        int j = numbers.length-1;
+        int[] copiNumber = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            copiNumber[j] = numbers[i];
+            j--;
+        }
+        numbers = copiNumber;
+        return numbers;
     }
 }
